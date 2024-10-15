9106ca3adc76df1a276f8dad8a1600ee61f8376b
Edward Brunetiere <57622377+P4tt4te@users.noreply.github.com>
2024-10-15 14:18:53 +0200

    History Integration (#2)
    * style: add HistoryResume
    
    * style: add HistoryFilters
    
    * style add HistoryList

---
+++ app/(tabs)/history.tsx
@@ -0,0 +1,47 @@
+import Header from "@/components/Header";
+import HistoryFilters from "@/components/HistoryFilters";
+import HistoryResume from "@/components/HistoryResume";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { StyleSheet } from "react-native";
+import { useState } from "react";
+import HistoryList from "@/components/HistoryList";
+
+export default function HistoryScreen() {
+  const [selectedFilter, setSelectedFilter] = useState(0);
+  const onFilterChange = (i: number) => {
+    setSelectedFilter(i);
+  };
+
+  return (
+    <ThemedView style={styles.container}>
+      <Header />
+      <ThemedView style={styles.content}>
+        <ThemedText style={styles.title}>Historique de mes reviews</ThemedText>
+        <ThemedView style={{ gap: 10 }}>
+          <HistoryResume />
+          <HistoryFilters
+            selectedFilter={selectedFilter}
+            onFilterChange={onFilterChange}
+          />
+          <HistoryList />
+        </ThemedView>
+      </ThemedView>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  content: {
+    flex: 1,
+    padding: 32,
+    gap: 16,
+  },
+  title: {
+    fontWeight: 700,
+    fontSize: 24,
+  },
+});
---
+++ components/HistoryList.tsx
@@ -0,0 +1,108 @@
+import { StyleSheet, ScrollView } from "react-native";
+import { Fragment } from "react";
+import { ThemedView } from "./ThemedView";
+import { ThemedText } from "./ThemedText";
+import { CustomColors } from "@/constants/Colors";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+
+function ListItem({
+  date,
+  social,
+  number,
+  status,
+}: {
+  date: string;
+  social: string;
+  number: string;
+  status: string;
+}) {
+    const isFinished = status === "finished";
+
+  return (
+    <ThemedView style={styles.listContainer}>
+      <ThemedText style={{ width: 120 }}>{date}</ThemedText>
+      <ThemedView>
+        <ThemedText>{social}</ThemedText>
+        <ThemedText>{`N°${number}`}</ThemedText>
+      </ThemedView>
+      <FontAwesome
+        style={[styles.icon, { right: isFinished ? 5 : 11 }]}
+        name={isFinished ? "check" : "question"}
+        size={24}
+        color={isFinished ? "green" : "orange"}
+      />
+    </ThemedView>
+  );
+}
+
+const FAKE_LIST = [
+  {
+    date: "31/03/2024",
+    social: "Instagram",
+    number: "201998",
+    status: "waiting",
+  },
+  {
+    date: "31/03/2024",
+    social: "Instagram",
+    number: "201998",
+    status: "finished",
+  },
+  {
+    date: "31/03/2024",
+    social: "Instagram",
+    number: "201998",
+    status: "finished",
+  },
+];
+
+export default function HistoryList() {
+  return (
+    <ScrollView showsHorizontalScrollIndicator={false}>
+      <ThemedView style={styles.header}>
+        <ThemedText style={[styles.headerText, { width: 120 }]}>
+          Date
+        </ThemedText>
+        <ThemedText style={styles.headerText}>Détail</ThemedText>
+      </ThemedView>
+      {FAKE_LIST.map((_, i) => (
+        <Fragment key={i}>
+          <ThemedView style={styles.divider}></ThemedView>
+          <ListItem
+            social={_.social}
+            date={_.date}
+            number={_.number}
+            status={_.status}
+          />
+        </Fragment>
+      ))}
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    gap: 5,
+  },
+  header: {
+    flexDirection: "row",
+  },
+  headerText: {
+    fontWeight: 600,
+  },
+  divider: {
+    height: 2,
+    width: "100%",
+    backgroundColor: CustomColors.grey,
+  },
+  listContainer: {
+    flexDirection: "row",
+    position: "relative",
+  },
+  icon: {
+    position: "absolute",
+    top: 5,
+    right: 5,
+  },
+});
