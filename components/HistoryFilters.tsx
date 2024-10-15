bf81052e0fc79115cee12c38cb52dd2b8b26a4b4
Edward B <57622377+P4tt4te@users.noreply.github.com>
2024-10-15 11:36:05 +0200

    style: add HistoryFilters

---
+++ components/HistoryFilters.tsx
@@ -0,0 +1,50 @@
+import { Pressable, StyleSheet } from "react-native";
+import { ThemedView } from "./ThemedView";
+import { ThemedText } from "./ThemedText";
+import { CustomColors } from "@/constants/Colors";
+
+interface HistoryFiltersProps {
+  selectedFilter: number;
+  onFilterChange: (filter: number) => void;
+}
+
+const FILTERS_LIST = ["Tous", "En attente", "Validé"];
+
+export default function HistoryFilters({
+  selectedFilter,
+  onFilterChange,
+}: HistoryFiltersProps) {
+  return (
+    <ThemedView style={styles.container}>
+      {FILTERS_LIST.map((_, i) => (
+        <Pressable
+          style={[
+            styles.filter,
+            {
+              backgroundColor:
+                selectedFilter === i ? CustomColors.grey : "transparent",
+            },
+          ]}
+          key={i}
+          onPress={() => onFilterChange(i)}
+        >
+          <ThemedText>{_}</ThemedText>
+        </Pressable>
+      ))}
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    gap: 8,
+  },
+  filter: {
+    borderWidth: 2,
+    borderColor: CustomColors.grey,
+    borderRadius: 10,
+    paddingHorizontal: 5,
+    paddingVertical: 2,
+  },
+});
