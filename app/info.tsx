0b9d1fb7559c2384829c17408effd677bbe068be
Edward Brunetiere <57622377+P4tt4te@users.noreply.github.com>
2024-10-15 10:38:55 +0200

    Home integration (#1)
    * feat: added Swiper component, and modified ParallaxScrollView
    
    * style: Update SwipeCard style & add info page
    
    * style: Create Custom Header

---
+++ app/info.tsx
@@ -0,0 +1,10 @@
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+
+export default function InfoScreen() {
+  return (
+    <ParallaxScrollView>
+      <ThemedText>INFO</ThemedText>
+    </ParallaxScrollView>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,32 @@
+import { ThemedView } from "./ThemedView";
+import { Bar } from "react-native-progress";
+import { StyleSheet } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { ThemedText } from "./ThemedText";
+import { CustomColors } from "@/constants/Colors";
+
+export default function Header() {
+  const insets = useSafeAreaInsets();
+
+  return (
+    <ThemedView style={[styles.container, { paddingTop: insets.top }]}>
+      <ThemedText style={styles.title}>200 XP</ThemedText>
+      <Bar width={200} progress={0.6} />
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    height: 70,
+    backgroundColor: CustomColors.grey,
+    justifyContent: "flex-end",
+    flexDirection: "row",
+    gap: 8,
+    alignItems: "center",
+    paddingHorizontal: 20,
+  },
+  title: {
+    fontWeight: 700,
+  },
+});
